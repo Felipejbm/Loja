5f2c20482971101c62b95542c95668c4d719cc4d
Felipejbm <felipejbmello.etec@gmail.com>
2025-08-26 09:02:48 -0300

    loja atualizações

---
+++ Loja/listar.php
@@ -0,0 +1,44 @@
+<?php
+include 'pedaco.php'
+?>
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Código do produto</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Preço</th>
+                <th scope="col">Estoque</th>
+                <th scope="col">Opções</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            require 'conexao.php';
+            $sql = "SELECT * FROM produtos";
+            $stmt = $pdo->query($sql);
+            while ($produto = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo "<tr>";
+                echo "<td>" . $produto['id'] . "</td>";
+                echo "<td>" . $produto['nome'] . "</td>";
+                echo "<td>" . $produto['preco'] . "</td>";
+                echo "<td>" . $produto['quantidade'] . "</td>";
+                echo "
+                <td>
+                    <div class='container'>
+                        <a href='form_atualizar.php? id= XXX' type='a' class='btn btn-outline-warning'>Atualizar</a>
+                        <a href='#' type='a' class='btn btn-outline-danger'>Apagar</a>
+                    </div>
+                </td>
+                ";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <a href="index.php" type="a" class="btn btn-secondary">Voltar ao menu</a>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
